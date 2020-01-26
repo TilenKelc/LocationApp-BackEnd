aa5cc0eb1f26371f4f56a9c2c860f1afdba095e7
fpv-life <40304752+fpv-life@users.noreply.github.com>
2020-01-26 20:35:57 +0100

    Upload

---
+++ app.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var axios = require('axios');
+var app = express();
+var cors = require('cors');
+
+app.set('view engine', 'html');
+app.use(cors());
+
+app.post('/', function(req, res){
+  var lat = [];
+  var lon = [];
+  var object = [];
+  var tempSave = [];
+
+  lat.push(req.query.coordinates1[0]);
+  lat.push(req.query.coordinates2[0]);
+
+  lon.push(req.query.coordinates1[1]);
+  lon.push(req.query.coordinates2[1]);
+  
+  axios('https://route.ls.hereapi.com/routing/7.2/calculateroute.json?' + 
+    'apiKey=ZxwsS89OjHMbyXLctmXRgOnugOgz1xgb9hGiwXdqTHg&' + 
+    'waypoint0=geo!' + lat[0] + ',' + lon[0] + '&' + 
+    'waypoint1=geo!' + lat[1] + ',' + lon[1] + '&mode=fastest;pedestrian;traffic:disabled')
+  .then((response)=>{
+    var coordinates = response.data.response.route[0].leg[0].maneuver;
+
+    axios('https://api.openweathermap.org/data/2.5/weather?lat=' + lat[0] + '&lon=' + lon[0] + '&units=metric&appid=39f1004c7ecc22d6f734974c44428625')
+    .then((response)=>{
+      object.push({lat: lat[0], lon: lon[0], temp: Math.round(response.data.main.temp), weather: response.data.weather[0].description});
+
+      axios('https://api.openweathermap.org/data/2.5/weather?lat=' + lat[1] + '&lon=' + lon[1] + '&units=metric&appid=39f1004c7ecc22d6f734974c44428625')
+      .then((response)=>{
+        object.push({lat: lat[1], lon: lon[1], temp: Math.round(response.data.main.temp), weather: response.data.weather[0].description});
+        
+        var locations = [];
+        var idx = coordinates.length;
+
+        coordinates.forEach(element => {
+          axios('https://api.openweathermap.org/data/2.5/weather?lat=' + element.position.latitude + '&lon=' + element.position.longitude + '&units=metric&appid=39f1004c7ecc22d6f734974c44428625')
+          .then((response)=>{
+
+            locations.push({lat: element.position.latitude, lon: element.position.longitude, temp: response.data.main.temp, weather: response.data.weather[0].description});
+
+            if (!--idx) {              
+              //locations.sort((a,b) => a.temp - b.temp);
+                        
+          
+              //console.log(coordinates.length);
+              //console.log(parseInt(coordinates.length/5));
+
+              var size = parseInt(locations.length/5);
+              
+              for (var a = 0; a < 5; a++) {
+                if(a == 0){
+                  var i = 0;
+                  var check = size;
+                }
+                else{
+                  check = check + size;
+                }
+                console.log(i + "|" + check)
+
+                for (; i < check; i++) {
+                  tempSave.push(locations[i]);
+                }
+                
+                tempSave.sort((a,b) => a.temp - b.temp);
+                tempSave = tempSave.slice(1).slice(-1);
+                object.push(tempSave[0]);
+
+                tempSave = [];
+              }
+
+              //console.log(object);
+              res.send({data: object});
+
+
+              //locations = locations.slice(1).slice(-5);            
+              /*
+              locations.forEach(element => {
+                object.push(element);
+              });
+              res.send({data: object});*/
+            }
+          })
+          .catch((error)=>{
+            console.log(error)
+            res.send({data: "Account temporary block"});
+          })
+        });
+      })
+      .catch((error)=>{
+        console.log(error)
+        res.send({data: "Account temporary block"});
+      })
+    })
+    .catch((error)=>{
+      console.log(error)
+      res.send({data: "Account temporary block"});
+    })
+  })
+  .catch((error)=>{
+    console.log(error)
+    res.send({data: "Account temporary block"});
+  })
+});
+
+app.listen(8080, function(mssg){
+  console.log('Listening to port 8080');
+});
